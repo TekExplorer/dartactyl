4e0c08985f870c6e98aa1c4a2a6b24bba234b2bf
Omar Kamel <tekexplorerm@gmail.com>
2024-06-24 21:49:25 -0400

    Version bumps

--- test/websocket/web_socket_channel_test.dart
+++ test/websocket/web_socket_channel_test.dart
@@ -2,9 +2,7 @@
 
 import 'dart:io';
 
-import 'package:stream_channel/stream_channel.dart';
 import 'package:test/test.dart';
-import 'package:web_socket_channel/web_socket_channel.dart';
 
 @TestOn('vm')
 //! copied from web_socket_channel tests
@@ -33,17 +31,16 @@ void main() {
 
     final response = await request.close();
     final socket = await response.detachSocket();
-    final innerChannel = StreamChannel<List<int>>(socket, socket);
-    final webSocket = WebSocketChannel(innerChannel, serverSide: false);
+    final webSocket = WebSocket.fromUpgradedSocket(socket, serverSide: false);
 
     var n = 0;
-    await webSocket.stream.listen((message) {
+    await webSocket.listen((message) {
       if (n == 0) {
         expect(message, equals('hello!'));
-        webSocket.sink.add('ping');
+        webSocket.add('ping');
       } else if (n == 1) {
         expect(message, equals('pong'));
-        webSocket.sink.close();
+        webSocket.close();
         server.close();
       } else {
         fail('Only expected two messages.');
